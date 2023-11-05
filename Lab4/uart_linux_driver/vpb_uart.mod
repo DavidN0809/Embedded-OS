15e56ad832abc78525ba014de00860a337d5e1ea
David Nichols <dnicho26@uncc.edu>
2023-11-04 20:58:04 -0400

    attempting todo cannot get displayout

---
+++ Lab4/uart_linux_driver/vpb_uart.mod
@@ -0,0 +1,1 @@
+/home/david/Documents/GitHub/Embedded-OS/Lab4/uart_linux_driver/vpb_uart.o
--- Lab4/uart_linux_driver/vpb_uart_test.c
+++ Lab4/uart_linux_driver/vpb_uart_test.c
@@ -1,17 +1,49 @@
 #include <stdio.h>
 #include <ctype.h>
 
+
 #define DEVNAME	"/dev/vpb_uart"
 
 int main(void) {
 	FILE *uart_dev;
 
 	uart_dev = fopen(DEVNAME, "r+");
-	fputc('A', uart_dev);
-	fputc('B', uart_dev);
-	fputc('C', uart_dev);
+	if (uart_dev == NULL) {
+        perror("Failed to open the UART device");
+        return -1;
+    }
+
+	fputc('a', uart_dev);
+	fputc('b', uart_dev);
+	fputc('c', uart_dev);
 
 	
 	fclose(uart_dev);
 	return 0;
 }
+
+/*
+int main(void) {
+    FILE *uart_dev;
+    int c;
+
+    uart_dev = fopen(DEVNAME, "r+");
+    if (uart_dev == NULL) {
+        perror("Failed to open the UART device");
+        return -1;
+    }
+
+    // Loop to echo characters
+    while (1) {
+        c = fgetc(uart_dev);  // Read a character from UART
+        if (feof(uart_dev) || ferror(uart_dev)) {
+            // Handle end of file or error
+            break;
+        }
+        fputc(c, uart_dev);    // Write the character back to UART
+    }
+
+    fclose(uart_dev);
+    return 0;
+}
+*/
